f9605a536f7dd353b60b830969b8fe1720cc043a
Tatiane Mossi <mossitatiane2@gmail.com>
2022-06-22 14:19:40 -0300

    Implementando a camada de domínio de funcionário

--- LocadoraVeiculos/LocadoraVeiculos.Dominio/ModuloFuncionario/Funcionario.cs
+++ LocadoraVeiculos/LocadoraVeiculos.Dominio/ModuloFuncionario/Funcionario.cs
@@ -1,8 +1,32 @@
 ﻿using LocadoraVeiculos.Dominio.Compartilhado;
+using System;
 
 namespace LocadoraVeiculos.Dominio.ModuloFuncionario
 {
     public class Funcionario : EntidadeBase<Funcionario>
     {
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataAdmissao { get; set; }
+        public string Salario { get; set; }
+        public bool EhAdmin { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            Funcionario funcionario = obj as Funcionario;
+
+            if (funcionario == null)
+                return false;
+
+            return
+                funcionario.Id.Equals(Id) &&
+                funcionario.Nome.Equals(Nome) &&
+                funcionario.Login.Equals(Login) &&
+                funcionario.Senha.Equals(Senha) &&
+                funcionario.DataAdmissao.Equals(DataAdmissao) &&
+                funcionario.Salario.Equals(Salario);
+                funcionario.EhAdmin.Equals(EhAdmin);
+        }
     }
 }
