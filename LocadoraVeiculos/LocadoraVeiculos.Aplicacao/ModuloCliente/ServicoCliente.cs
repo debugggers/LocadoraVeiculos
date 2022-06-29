02e8d7749025117a5d6eda51b3ecabdfd8642e4a
Paulo Camargo <paulo13765@outlook.com>
2022-06-29 14:21:02 -0300

    Inicio da implementação da camada de aplicação

---
+++ LocadoraVeiculos/LocadoraVeiculos.Aplicacao/ModuloCliente/ServicoCliente.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculos.Aplicacao.ModuloCliente
+{
+    public class ServicoCliente
+    {
+
+        public ValidationResult Inserir()
+        {
+
+            return new ValidationResult();
+
+        }
+
+    }
+}
