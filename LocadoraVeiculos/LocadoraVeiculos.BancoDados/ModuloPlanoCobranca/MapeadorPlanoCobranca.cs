7f7fe3d5c0b7b7f7b50e790a6f5d36aafa434727
Tatiane Mossi <mossitatiane2@gmail.com>
2022-06-30 18:15:52 -0300

    Implementado camada infra do módulo de plano de cobrança.

---
+++ LocadoraVeiculos/LocadoraVeiculos.BancoDados/ModuloPlanoCobranca/MapeadorPlanoCobranca.cs
@@ -0,0 +1,44 @@
+﻿using LocadoraVeiculos.BancoDados.Compartilhado;
+using LocadoraVeiculos.Dominio.ModuloGrupoVeiculos;
+using LocadoraVeiculos.Dominio.ModuloPlanoCobranca;
+using System;
+using System.Data.SqlClient;
+
+namespace LocadoraVeiculos.BancoDados.ModuloPlanoCobranca
+{
+    public class MapeadorPlanoCobranca : MapeadorBase<PlanoCobranca>
+    {
+        public override PlanoCobranca ConverterRegistro(SqlDataReader leitorPlanoCobranca)
+        {
+            var id = Convert.ToInt32(leitorPlanoCobranca["ID"]);
+            var tipoPlano = (TipoPlanoEnum)(leitorPlanoCobranca["TIPOPLANO"]);
+            var valor = Convert.ToDecimal(leitorPlanoCobranca["VALOR"]);
+            
+            var grupoVeiculos_Id = Convert.ToInt32(leitorPlanoCobranca["GRUPOVEICULOS_ID"]);
+            var grupoVeiculos_Nome = Convert.ToString(leitorPlanoCobranca["GRUPOVEICULOS_NOME"]);
+
+            var planoCobranca = new PlanoCobranca()
+            {
+                Id = id,
+                TipoPlano = tipoPlano,
+                Valor = valor,
+                IdGrupoVeiculos = grupoVeiculos_Id,
+                GrupoVeiculos = new GrupoVeiculos
+                {
+                    Id = grupoVeiculos_Id,
+                    Nome = grupoVeiculos_Nome
+                }                
+            };
+
+            return planoCobranca;
+        }
+
+        public override void ConfigurarParametros(PlanoCobranca novoPlanoCobranca, SqlCommand comando)
+        {
+            comando.Parameters.AddWithValue("ID", novoPlanoCobranca.Id);
+            comando.Parameters.AddWithValue("TIPOPLANO", novoPlanoCobranca.TipoPlano);
+            comando.Parameters.AddWithValue("VALOR", novoPlanoCobranca.Valor);
+            comando.Parameters.AddWithValue("GRUPOVEICULOS_ID", novoPlanoCobranca.IdGrupoVeiculos);
+        }
+    }
+}
---
+++ LocadoraVeiculos/LocadoraVeiculos.BancoDados/ModuloPlanoCobranca/RepositorioPlanoCobrancaEmBancoDados.cs
@@ -0,0 +1,61 @@
+﻿using ControleMedicamentos.Infra.BancoDados.Compartilhado;
+using LocadoraVeiculos.Dominio.ModuloPlanoCobranca;
+using System.Data.SqlClient;
+
+namespace LocadoraVeiculos.BancoDados.ModuloPlanoCobranca
+{
+    public class RepositorioPlanoCobrancaEmBancoDados :
+        RepositorioBase<PlanoCobranca, MapeadorPlanoCobranca>
+    {
+        #region SQL Queries
+        protected override string sqlInserir =>
+            @"INSERT INTO TBPLANOCOBRANCA
+            (
+                    TIPOPLANO,
+                    VALOR,
+                    GRUPOVEICULOS_ID
+            )
+            VALUES
+            (
+                    @TIPOPLANO,
+                    @VALOR,
+                    @GRUPOVEICULOS_ID
+            );SELECT SCOPE_IDENTITY();";
+
+        protected override string sqlEditar =>
+           @"UPDATE TBPLANOCOBRANCA
+		        SET
+			        TIPOPLANO = @TIPOPLANO,
+                    VALOR = @VALOR,
+                    GRUPOVEICULOS_ID = @GRUPOVEICULOS_ID
+		        WHERE
+			        ID = @ID";
+
+        protected override string sqlExcluir =>
+            @"DELETE FROM TBPLANOCOBRANCA
+		        WHERE
+			        ID = @ID";
+
+        protected override string sqlSelecionarPorId =>
+            @"SELECT * FROM TBPLANOCOBRANCA
+		        WHERE
+                    ID = @ID";
+
+        protected override string sqlSelecionarTodos =>
+             @"SELECT * FROM TBPLANOCOBRANCA";
+
+        private const string sqlSelecionarPorTipoPlano =
+            @"SELECT * 
+                FROM 
+                    TBPLANOCOBRANCA
+                WHERE
+                    TIPOPLANO = @TIPOPLANO";
+
+        #endregion
+
+        public PlanoCobranca SelecionarPlanoCobrancaPorTipoPlano(string tipoPlano)
+        {
+            return SelecionarPorParametro(sqlSelecionarPorTipoPlano, new SqlParameter("TIPOPLANO", tipoPlano));
+        }
+    }
+}
