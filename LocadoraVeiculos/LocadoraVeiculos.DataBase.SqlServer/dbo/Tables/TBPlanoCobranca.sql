fa93d6a0065ab11acee3a6da3f6986ac97a071f6
Tatiane Mossi <mossitatiane2@gmail.com>
2022-07-04 14:03:23 -0300

    Criado tabela plano de cobrança no projeto.

---
+++ LocadoraVeiculos/LocadoraVeiculos.DataBase.SqlServer/dbo/Tables/TBPlanoCobranca.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[TBPlanoCobranca](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ValorDiario_Diario] [decimal](5, 2) NOT NULL,
+	[ValorPorKm_Diario] [decimal](5, 2) NOT NULL,
+	[ValorDiario_Livre] [decimal](5, 2) NOT NULL,
+	[ValorDiario_Controlado] [decimal](5, 2) NOT NULL,
+	[ValorPorKm_Controlado] [decimal](5, 2) NOT NULL,
+	[ControleKm_Controlado] [int] NOT NULL,
+	[GrupoVeiculos_Id] [int] NOT NULL, 
+    CONSTRAINT [PK_TBPlanoCobranca] PRIMARY KEY ([Id]),
+	CONSTRAINT [FK_TBPlanoCobranca_TBGrupoVeiculo] FOREIGN KEY ([GrupoVeiculos_Id]) REFERENCES [dbo].[TBGrupoVeiculo] ([Id])
+);
+ 
+
+
