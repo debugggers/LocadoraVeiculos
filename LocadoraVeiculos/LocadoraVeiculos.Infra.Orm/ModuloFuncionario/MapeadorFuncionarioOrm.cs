9d3db8a3da35fc6c515e5894d8ed1d73c4a494d7
Paulo Camargo <paulo13765@outlook.com>
2022-07-26 14:27:35 -0300

    merge

--- LocadoraVeiculos/LocadoraVeiculos.Infra.Orm/ModuloFuncionario/MapeadorFuncionarioOrm.cs
+++ LocadoraVeiculos/LocadoraVeiculos.Infra.Orm/ModuloFuncionario/MapeadorFuncionarioOrm.cs
@@ -4,11 +4,21 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace LocadoraVeiculos.Infra.Orm.ModuloFuncionario
 {
-    internal class MapeadorFuncionarioOrm : IEntityTypeConfiguration<Funcionario>
+    public class MapeadorFuncionarioOrm : IEntityTypeConfiguration<Funcionario>
     {
+
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
         public void Configure(EntityTypeBuilder<Funcionario> builder)
         {
-            throw new System.NotImplementedException();
+            builder.ToTable("TBFuncionario");
+            builder.Property(x => x.Id).ValueGeneratedNever();
+            builder.Property(x => x.Nome).IsRequired().HasColumnType("Varchar(100)");
+            builder.Property(x => x.Login).IsRequired().HasColumnType("Varchar(100)");
+            builder.Property(x => x.Senha).IsRequired().HasColumnType("Varchar(100)");
+            builder.Property(x => x.DataAdmissao).IsRequired();
+            builder.Property(x => x.Salario).IsRequired();
+            builder.Property(x => x.EhAdmin).IsRequired();
         }
     }
 }
