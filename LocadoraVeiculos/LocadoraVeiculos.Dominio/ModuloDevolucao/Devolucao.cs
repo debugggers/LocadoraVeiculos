c7c6eb09881f08b689cf598dcadd17ab84f26dd2
Paulo Camargo <paulo13765@outlook.com>
2022-07-27 17:20:42 -0300

    implementado camada de dominio e  tela  de cadastro de devoluções.

---
+++ LocadoraVeiculos/LocadoraVeiculos.Dominio/ModuloDevolucao/Devolucao.cs
@@ -0,0 +1,50 @@
+﻿using LocadoraVeiculos.Dominio.Compartilhado;
+using LocadoraVeiculos.Dominio.ModuloLocacao;
+using LocadoraVeiculos.Dominio.ModuloTaxa;
+using System;
+using System.Collections.Generic;
+
+namespace LocadoraVeiculos.Dominio.ModuloDevolucao
+{
+    public class Devolucao : EntidadeBase<Devolucao>
+    {
+
+        public Locacao Locacao { get; set; }
+        public int QuilometragemVeiculo { get; set; }
+        public DateTime DataDevolucao { get; set; }
+        public  Double NivelDoTanque { get; set; }
+        public  List<Taxa> Taxas { get; set; }
+        public Double ValorTotal { get; set; }
+
+        public Devolucao()
+        {
+        }
+
+        public Devolucao(Locacao locacao, int quilometragemVeiculo, DateTime dataDevolucao, double nivelDoTanque, List<Taxa> taxas, double valorTotal)
+        {
+            Locacao = locacao;
+            QuilometragemVeiculo = quilometragemVeiculo;
+            DataDevolucao = dataDevolucao;
+            NivelDoTanque = nivelDoTanque;
+            Taxas = taxas;
+            ValorTotal = valorTotal;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Devolucao devolucao &&
+                  Id == devolucao.Id &&
+                  Locacao == devolucao.Locacao &&
+                  QuilometragemVeiculo == devolucao.QuilometragemVeiculo &&
+                  DataDevolucao == devolucao.DataDevolucao &&
+                  NivelDoTanque == devolucao.NivelDoTanque &&
+                  Taxas == devolucao.Taxas &&
+                  ValorTotal == devolucao.ValorTotal;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ LocadoraVeiculos/WinFormsApp1/ModuloDevolucao/ConfiguracaoToolBoxDevolucao.cs
@@ -0,0 +1,16 @@
+﻿using LocadoraVeiculosForm.Compartilhado;
+using System;
+
+namespace LocadoraVeiculosForm.ModuloDevolucao
+{
+    public class ConfiguracaoToolBoxDevolucao : ConfiguracaoToolboxBase
+    {
+        public override string TipoCadastro => throw new NotImplementedException();
+
+        public override string TooltipInserir => throw new NotImplementedException();
+
+        public override string TooltipEditar => throw new NotImplementedException();
+
+        public override string TooltipExcluir => throw new NotImplementedException();
+    }
+}
