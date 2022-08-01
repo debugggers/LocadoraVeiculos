7a3782f7b0e73b0085879c8a3bbb66bd6ac7273a
Paulo Camargo <paulo13765@outlook.com>
2022-08-01 15:58:36 -0300

    corrigido testes unitários

--- LocadoraVeiculos/LocadoraVeiculos.Infra.Dominio.TestesUnitarios/ModuloDevolucao/DevolucaoTeste.cs
+++ LocadoraVeiculos/LocadoraVeiculos.Infra.Dominio.TestesUnitarios/ModuloDevolucao/DevolucaoTeste.cs
@@ -1,4 +1,6 @@
 ﻿using LocadoraVeiculos.Dominio.ModuloDevolucao;
+using LocadoraVeiculos.Dominio.ModuloLocacao;
+using LocadoraVeiculos.Dominio.ModuloVeiculo;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LocadoraVeiculos.Infra.Dominio.TestesUnitarios.ModuloDevolucao
@@ -12,30 +14,48 @@ namespace LocadoraVeiculos.Infra.Dominio.TestesUnitarios.ModuloDevolucao
         {
             Devolucao devolucao = new Devolucao();
 
+            Locacao locacao = new Locacao();
+
+            Veiculo veiculo = new Veiculo();
+
+            veiculo.QuilometragemPercorrida = 500;
+
+            locacao.Veiculo = veiculo;
+
+            devolucao.Locacao = locacao;
+
             ValidadorDevolucao validador = new ValidadorDevolucao();
 
             var resultadoValidacao = validador.Validate(devolucao);
 
-            Assert.AreEqual("O campo locação não pode ficar vazio", resultadoValidacao.Errors[0].ErrorMessage);
-            Assert.AreEqual("O campo quilometragem não pode ficar vazio", resultadoValidacao.Errors[2].ErrorMessage);
-            Assert.AreEqual("O campo data de devolução não pode ficar vazio", resultadoValidacao.Errors[4].ErrorMessage);
-            Assert.AreEqual("O campo nivel do tanque não pode ficar vazio", resultadoValidacao.Errors[5].ErrorMessage);
+            Assert.AreEqual("O campo quilometragem não pode ficar vazio", resultadoValidacao.Errors[0].ErrorMessage);
+            Assert.AreEqual("O campo data de devolução não pode ficar vazio", resultadoValidacao.Errors[2].ErrorMessage);
+            Assert.AreEqual("O campo nivel do tanque não pode ficar vazio", resultadoValidacao.Errors[4].ErrorMessage);
 
         }
 
         [TestMethod]
-        public void Campo_Quilometragem_Deve_Ser_Maior_Do_Que_0()
+        public void Campo_Quilometragem_Deve_Ser_Maior_Do_Que_A_Anterior()
         {
             Devolucao devolucao = new Devolucao();
 
-            devolucao.QuilometragemVeiculo = -10;
+            Locacao locacao = new Locacao();
+
+            Veiculo veiculo = new Veiculo();
+
+            veiculo.QuilometragemPercorrida = 500;
+
+            locacao.Veiculo = veiculo;
+
+            devolucao.Locacao = locacao;
+
+            devolucao.QuilometragemVeiculo = 250;
 
             ValidadorDevolucao validador = new ValidadorDevolucao();
 
             var resultadoValidacao = validador.Validate(devolucao);
 
-            Assert.AreEqual("A quilometragem deve ser maior do que 0", resultadoValidacao.Errors[2].ErrorMessage);
-
+            Assert.AreEqual("A quilometragem atual deve ser maior do que a anterior a locação", resultadoValidacao.Errors[0].ErrorMessage);
         }
     }
 }
