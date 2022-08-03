0ca483ea7ee72d34da7bd25f68d5ed15beed3189
Paulo Camargo <paulo13765@outlook.com>
2022-08-03 19:59:34 -0300

    criado estrutura inicial para implementação da geração de pdfs

---
+++ LocadoraVeiculos/LocadoraVeiculos.Dominio/Compartilhado/IGeradorPdf.cs
@@ -0,0 +1,9 @@
+﻿namespace LocadoraVeiculos.Dominio.Compartilhado
+{
+    public interface IGeradorPdf<T> where T : EntidadeBase<T>
+    {
+
+        public void GerarPdf(T comprovante);
+
+    }
+}
---
+++ LocadoraVeiculos/LocadoraVeiculos.Dominio/ModuloLocacao/IGeradorPdfLocacao.cs
@@ -0,0 +1,11 @@
+﻿using LocadoraVeiculos.Dominio.Compartilhado;
+
+namespace LocadoraVeiculos.Dominio.ModuloLocacao
+{
+    public interface IGeradorPdfLocacao : IGeradorPdf<Locacao>
+    {
+
+
+
+    }
+}
